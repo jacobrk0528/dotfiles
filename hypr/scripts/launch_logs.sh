fecc81f40fcabc26b72bd67dce04ad7456b349d6
Jacob <jkrebs@trinityroad.com>
2026-05-24 23:07:04 -0400

    hypr: update config, add lua config, and refactor log launcher

--- hypr/scripts/launch_logs.sh
+++ hypr/scripts/launch_logs.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-WORKSPACE="special:logs"
 SLEEP_TIME=.5
 
 # bc utils logs
-hyprctl dispatch exec "[workspace $WORKSPACE] ghostty --title=bc_utils_log -e bash -c \"ssh -t bespin 'tail -f /var/log/bc_cron.log'; echo 'Command exited. Press enter to close.'; read\""
+ghostty --title=bc_utils_log -e bash -c "ssh -t bespin 'tail -f /var/log/bc_cron.log'; echo 'Command exited. Press enter to close.'; read" &
 sleep $SLEEP_TIME
 
 # narsil logs
-hyprctl dispatch exec "[workspace $WORKSPACE] ghostty --title=narsil_log -e bash -c \"ssh -t dagobah 'tail -f /var/log/narsil.log'; echo 'Command exited. Press enter to close.'; read\""
-sleep $SLEEP_TIME
+ghostty --title=narsil_log -e bash -c "ssh -t dagobah 'tail -f /var/log/narsil.log'; echo 'Command exited. Press enter to close.'; read" &
